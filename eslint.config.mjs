00141e4cf63873bc8ff542dcf51adf609a2d1d4b
darahub <229756729+darahub@users.noreply.github.com>
2026-08-28 22:40:41 +0100

    chore: scaffold pnpm TypeScript workspace

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    ignores: [
+      "**/.next/**",
+      "**/dist/**",
+      "**/coverage/**",
+      "**/node_modules/**",
+    ],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+];
